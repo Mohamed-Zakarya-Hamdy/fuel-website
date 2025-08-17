cbeb77677144a136fbac76c5ea3061ff33fae4c6
Mohamed-Zakarya-Hamdy <mohamed.zakarya6777@gmail.com>
2025-08-17 16:36:10 +0300

    Initial commit

---
+++ src/pages/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Login() {
+  return <h1>🔑 Login Page</h1>;
+}
+
+export default Login;
---
+++ src/pages/Signup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Signup() {
+  return <h1>📝 Signup Page</h1>;
+}
+
+export default Signup;
